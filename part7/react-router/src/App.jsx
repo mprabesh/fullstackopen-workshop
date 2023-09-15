3c1ca3475a8cc50cf45b1d726584420af276809d
mprabesh <theprabesh723@gmail.com>
2023-09-15 12:05:42 +0545

    feat: part7 excercise setup react-router

---
+++ part7/react-router/src/App.jsx
@@ -0,0 +1,52 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const Home = () => (
+  <div>
+    {" "}
+    <h2>TKTL notes app</h2>{" "}
+  </div>
+);
+
+const Notes = () => (
+  <div>
+    {" "}
+    <h2>Notes</h2>{" "}
+  </div>
+);
+
+const Users = () => (
+  <div>
+    {" "}
+    <h2>Users</h2>{" "}
+  </div>
+);
+
+const App = () => {
+  const padding = {
+    padding: 15,
+  };
+
+  return (
+    <BrowserRouter>
+      <div>
+        <Link style={padding} to="/">
+          home
+        </Link>
+        <Link style={padding} to="/notes">
+          notes
+        </Link>
+        <Link style={padding} to="/users">
+          users
+        </Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
