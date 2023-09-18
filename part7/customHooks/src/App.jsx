2eb8566e7421547d7fee80c3e9169ccf2d7beb7e
mprabesh <theprabesh723@gmail.com>
2023-09-18 12:00:51 +0545

    feat: two counter tracker

--- part7/customHooks/src/App.jsx
+++ part7/customHooks/src/App.jsx
@@ -1,14 +1,23 @@
 import { useCounter } from "./hooks/useCounter";
 const App = () => {
-  const { count, increase, decrease, zero } = useCounter();
-
+  const leftCounter = useCounter();
+  const rightCounter = useCounter();
   return (
-    <div>
-      <div>{count}</div>
-      <button onClick={increase}>plus</button>
-      <button onClick={decrease}>minus</button>
-      <button onClick={zero}>zero</button>
-    </div>
+    <>
+      <div>
+        <div>{leftCounter.count}</div>
+        <button onClick={leftCounter.increase}>plus</button>
+        <button onClick={leftCounter.decrease}>minus</button>
+        <button onClick={leftCounter.zero}>zero</button>
+      </div>
+      <hr />
+      <div>
+        <div>{rightCounter.count}</div>
+        <button onClick={rightCounter.increase}>plus</button>
+        <button onClick={rightCounter.decrease}>minus</button>
+        <button onClick={rightCounter.zero}>zero</button>
+      </div>
+    </>
   );
 };
 
