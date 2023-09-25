289a86881504a3801089ef886e983d13c6ce6376
mprabesh <theprabesh723@gmail.com>
2023-09-25 14:26:20 +0545

    feat:sequelize model used

--- part13/notes-app/index.js
+++ part13/notes-app/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Sequelize, QueryTypes } = require("sequelize");
+const { Sequelize, Model, QueryTypes, DataTypes } = require("sequelize");
 const express = require("express");
 const app = express();
 
@@ -16,21 +16,48 @@ const sequelize = new Sequelize(process.env.DB_URL, {
   },
 });
 
+class Note extends Model {}
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    important: {
+      type: DataTypes.BOOLEAN,
+    },
+    date: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "note",
+  }
+);
+
 app.get("/api/notes", async (req, res) => {
-  const notes = await sequelize.query("SELECT * FROM notes", {
-    type: QueryTypes.SELECT,
-  });
+  const notes = await Note.findAll();
   res.status(200).json(notes);
+});
+
+app.get("/api/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const note = await Note.findByPk(id);
+  res.status(200).json(note);
 });
 
 app.post("/api/notes", async (req, res) => {
   const newNote = req.body;
-  console.log(newNote);
   try {
-    const response = await sequelize.query(
-      `insert into notes (content, important) values ('${newNote.content}', ${newNote.important})`,
-      { type: QueryTypes.INSERT }
-    );
+    const reponse = await Note.create(newNote);
     res.json(response);
   } catch (error) {
     res.status(400).send(error);
