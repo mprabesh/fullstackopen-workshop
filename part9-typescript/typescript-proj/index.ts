496070207e6b51d4fb870c028bf520cd93c41195
mprabesh <theprabesh723@gmail.com>
2023-10-04 13:34:33 +0545

    feat:calculator func

--- part9-typescript/typescript-proj/index.ts
+++ part9-typescript/typescript-proj/index.ts
@@ -1,16 +1,26 @@
 type Operation = "add" | "multiply" | "divide";
+type Result = string | number;
 
-function multiplier(num1: number, num2: number, op: Operation) {
-  if (op === "add") {
-    return num1 + num2;
-  } else if (op === "divide") {
-    if (num2 === 0) {
-      return "can't divide by zero";
-    }
-    return num1 / num2;
-  } else if (op === "multiply") {
-    return num1 * num2;
+function calculator(num1: number, num2: number, op: Operation): Result {
+  switch (op) {
+    case "multiply":
+      return num1 * num2;
+    case "add":
+      return num1 + num2;
+    case "divide":
+      if (num2 === 0) return "Can't divide by zero";
+      return num1 / num2;
+    default:
+      throw new Error("Operation is not multiply, add or divide!");
   }
 }
 
-console.log(multiplier(12, 4, "divide"));
+try {
+  console.log(calculator(12, 3, "divide"));
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong!";
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
