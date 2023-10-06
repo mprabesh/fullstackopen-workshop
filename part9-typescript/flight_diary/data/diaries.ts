36ce27d67aa9cc3e4f91e355e58b20bf85e97445
mprabesh <theprabesh723@gmail.com>
2023-10-06 08:33:45 +0545

    feat:enum type added

--- part9-typescript/flight_diary/data/diaries.ts
+++ part9-typescript/flight_diary/data/diaries.ts
@@ -1,18 +1,18 @@
 import { DiaryEntry } from "../src/types";
-
+import { Weather, Visibility } from "../src/types";
 const diaryEntries: DiaryEntry[] = [
   {
     id: 1,
     date: "2017-01-01",
-    weather: "rainy",
-    visibility: "poor",
+    weather: Weather.Sunny,
+    visibility: Visibility.POOR,
     comment: "Pretty scary flight, I'm glad I'm alive",
   },
   {
     id: 2,
     date: "2017-04-01",
-    weather: "sunny",
-    visibility: "good",
+    weather: Weather.Sunny,
+    visibility: Visibility.GOOD,
     comment: "Everything went better than expected, I'm learning much",
   },
 ];
