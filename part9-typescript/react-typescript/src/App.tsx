e5013a52e892097045012e97c0fd606ee2f6f045
mprabesh <theprabesh723@gmail.com>
2023-10-06 16:08:48 +0545

    feat:notes app using typescript

--- part9-typescript/react-typescript/src/App.tsx
+++ part9-typescript/react-typescript/src/App.tsx
@@ -1,51 +1,36 @@
-import DATA from "./data.ts";
-import { CoursePart } from "./types.ts";
-
-interface WelcomeProps {
-  name: string;
-}
-
-const Welcome = (props: WelcomeProps): JSX.Element => {
-  return <h1>Hello, {props.name}</h1>;
-};
-
-const assertNever = (value: never): never => value;
+import { useEffect, useState } from "react";
+import { Note } from "./types";
+import { getAllNotes, createNote } from "./noteService";
 
 const App = () => {
-  const courseName = "Half Stack application development";
-  const courseParts: CoursePart[] = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10,
-      description: "This is an awesome course part",
-      kind: "basic",
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7,
-      groupProjectCount: 3,
-      kind: "group",
-    },
-    {
-      name: "Basics of type Narrowing",
-      exerciseCount: 7,
-      description: "How to go from unknown to string",
-      kind: "basic",
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14,
-      description: "Confusing description",
-      backgroundMaterial:
-        "https://type-level-typescript.com/template-literal-types",
-      kind: "background",
-    },
-  ];
+  const [newNote, setNewNote] = useState("");
+  const [notes, setNotes] = useState<Note[]>([{ id: 1, content: "testing" }]);
+  const handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    createNote({ content: newNote }).then((data) => setNotes([...notes, data]));
+    setNewNote("");
+  };
+
+  useEffect(() => {
+    getAllNotes().then((data) => setNotes(data));
+  }, []);
 
   return (
-    <>
-      <Welcome name="Sarah" />
-    </>
+    <div>
+      <h2>Note App with TS</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
--- part9-typescript/react-typescript/src/types.ts
+++ part9-typescript/react-typescript/src/types.ts
@@ -1,25 +1,6 @@
-interface CoursePartBase {
-  name: string;
-  exerciseCount: number;
+export interface Note {
+  id: number;
+  content: string;
 }
 
-interface CoursePartBasic extends CoursePartBase {
-  description: string;
-  kind: "basic";
-}
-
-export interface CoursePartGroup extends CoursePartBase {
-  groupProjectCount: number;
-  kind: "group";
-}
-
-export interface CoursePartBackground extends CoursePartBase {
-  description: string;
-  backgroundMaterial: string;
-  kind: "background";
-}
-
-export type CoursePart =
-  | CoursePartBasic
-  | CoursePartGroup
-  | CoursePartBackground;
+export type NewNote = Omit<Note, "id">;
