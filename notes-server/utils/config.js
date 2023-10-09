11ce520eb5e6cf7d5c330776ad25e6632a037190
mprabesh <theprabesh723@gmail.com>
2023-10-09 11:08:17 +0545

    feat:del changes

--- notes-server/utils/config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const url =
-  process.env.NODE_ENV === "test"
-    ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI;
-const PORT = process.env.PORT;
-const SECRET_KEY = process.env.SECRET_KEY;
-module.exports = { url, PORT, SECRET_KEY };
--- part13/notes-app/index.js
+++ part13/notes-app/index.js
@@ -1,94 +1,16 @@
-require("dotenv").config();
-const { Sequelize, Model, DataTypes } = require("sequelize");
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT ? process.env.PORT : 3002;
+const { PORT } = require("./utils/config");
+const { connectToDatabase } = require("./utils/db");
 
 app.use(express.json());
 
-const sequelize = new Sequelize(process.env.DB_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-class Note extends Model {}
-Note.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    important: {
-      type: DataTypes.BOOLEAN,
-    },
-    date: {
-      type: DataTypes.DATE,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "note",
-  }
-);
-
-// this creates DB if it does not exists
-Note.sync();
-
-app.get("/api/notes", async (req, res) => {
-  const notes = await Note.findAll();
-  res.status(200).json(notes);
-});
-
-app.get("/api/notes/:id", async (req, res) => {
-  const id = req.params.id;
-  const note = await Note.findByPk(id);
-  if (note) {
-    // console.log(note.toJSON());
-    console.log(JSON.stringify(note, null, 13));
-    res.status(200).json(note);
-  } else {
-    res.status(401).json({ error: "requested note not found" });
-  }
-});
-
-app.post("/api/notes", async (req, res) => {
-  const newNote = req.body;
-  try {
-    const data = await Note.create(newNote);
-    res.json(data);
-  } catch (error) {
-    res.status(400).send({ error });
-  }
-});
-
-app.put("/api/notes/:id", async (req, res) => {
-  const id = req.params.id;
-  try {
-    const note = await Note.findByPk(id);
-    if (note) {
-      note.important = req.body.important;
-      await note.save();
-      res.json(note);
-    } else {
-      res.status(401).json({ error: "requested note not found" });
-    }
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}`);
-});
+start();
